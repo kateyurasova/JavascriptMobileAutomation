c8add99d612726d6cef2894c05ef94601ca654c9
katsiaryna.yurasova <kateyurasova@gmail.com>
2018-07-06 14:36:28 +0300

    Changed Configuration

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,22 +1,37 @@
 //jshint strict: false
 exports.config = {
 
-  allScriptsTimeout: 11000,
+    allScriptsTimeout: 11000,
 
-  specs: [
-    '*.js'
-  ],
+    specs: [
+        '*.js'
+    ],
 
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    capabilities: {
+        'browserName': 'chrome'
+    },
 
-  baseUrl: 'http://localhost:8000/',
+    baseUrl: 'http://localhost:8000/',
 
-  framework: 'jasmine',
+    framework: 'jasmine',
 
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    },
+
+    onPrepare: function () {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+        jasmine.getEnv().afterEach(function(done){
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            })
+        });
+    }
 
 };
