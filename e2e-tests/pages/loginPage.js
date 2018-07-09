a3e05d1656ba1e0930f4dbd3010d123bfe0d39e9
katsiaryna.yurasova <kateyurasova@gmail.com>
2018-07-09 17:07:37 +0300

    Changed page objects

--- e2e-tests/pages/loginPage.js
+++ e2e-tests/pages/loginPage.js
@@ -1,36 +1,29 @@
 require('../pages/userPage.js')
 
 let loginPage = function () {
+    let loginInput = element(by.name("loginForm.login"))
+    let loginPassword = element(by.name("loginForm.password"))
+    let okButton = element(by.name('ok'))
 
     this.getURL = function () {
-        var currentURL = browser.getCurrentUrl()
-        return currentURL;
+        let currentURL = browser.getCurrentUrl()
+        return currentURL
 
     }
 
-    this.enterUserLogin = function (loginValue) {
-        element(by.name("loginForm.login")).sendKeys(loginValue)
+    this.enterUserLogin = async function (loginValue) {
+        await loginInput.sendKeys(loginValue)
 
     }
 
-    this.enterUserPassword = function (passwordValue) {
-        element(by.name("loginForm.password")).sendKeys(passwordValue)
-    }
-
-    this.clickOkButton = async function () {
-        await element(by.name('ok')).click()
-
+    this.enterUserPassword = async function (passwordValue) {
+        await loginPassword.sendKeys(passwordValue)
     }
 
     this.logIn = async function (userLogin, userPassword) {
-
-        let EC = protractor.ExpectedConditions
-        browser.wait(EC.presenceOf(element(by.name("loginForm.login"))), 10000,
-            "field for entering of login did no appear in 10 sec")
-
-        await element(by.name("loginForm.login")).sendKeys(userLogin)
-        await element(by.name("loginForm.password")).sendKeys(userPassword)
-        await element(by.name('ok')).click()
+        this.enterUserLogin(userLogin)
+        this.enterUserPassword(userPassword)
+        await okButton.click()
     }
 
 }
--- e2e-tests/pages/mainPage.js
+++ e2e-tests/pages/mainPage.js
@@ -1,12 +1,9 @@
 require('../pages/loginPage.js')
 
 let mainPage = function () {
+    let loginLink = element(by.xpath('//a[@href="/LoginAction.loginForm"]'))
     this.clickLogIn = async function () {
-        let loginLink = element(by.xpath('//a[@href="/LoginAction.loginForm"]'))
-        let EC = protractor.ExpectedConditions
-        browser.wait(EC.presenceOf(element(by.xpath('//a[@href="/LoginAction.loginForm"]'))), 10000,
-            "Login link did no appear at the Main page in 10 seconds")
-        await element(by.xpath('//a[@href="/LoginAction.loginForm"]')).click()
+        await loginLink.click()
     }
 }
 
