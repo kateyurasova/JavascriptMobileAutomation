2cd84aa9b16b393e99e66e743f01872e4ecafaaa
katsiaryna.yurasova <kateyurasova@gmail.com>
2018-07-10 14:12:26 +0300

    Added step

--- e2e-tests/tests/loginPageSpec.js
+++ e2e-tests/tests/loginPageSpec.js
@@ -1,10 +1,13 @@
+
 describe('opening of the userPage with using of PageObject Pattern', function () {
 
     const BASE_URL = 'http://www.quizful.net/test'
     const USER_LOGIN = "MariaMitko"
     const USER_PASSWORD = "ZcysqGthtw13"
     const USER_NAME = "MariaMitko"
-
+    const log4js = require('log4js')
+    let logger = log4js.getLogger()
+    logger.level = 'info'
 
     let mainPage = require('../pages/mainPage.js')
     let loginPage = require('../pages/loginPage.js')
@@ -12,17 +15,18 @@ describe('opening of the userPage with using of PageObject Pattern', function ()
 
     beforeEach(async function () {
         browser.ignoreSynchronization = true
+        logger.info('GIVEN User is at the main page')
         browser.get(BASE_URL)
+        logger.info('WHEN user performs log in')
         await mainPage.clickLogIn()
         await loginPage.logIn(USER_LOGIN, USER_PASSWORD)
 
     })
 
     it('checking of the UserName', async function () {
+        logger.info('THEN Verify that user name is correct')
         let userName = userPage.getUserName()
-        allure.createStep('[{0}] Saving the settings', async function () {
-            await expect(userName).toEqual(USER_NAME, `Actual user name is not equal to expected value: ${USER_NAME}`)
-        })
+        await expect(userName).toEqual(USER_NAME, `Actual user name is not equal to expected value: ${USER_NAME}`)
     })
 
     afterEach(async function () {
