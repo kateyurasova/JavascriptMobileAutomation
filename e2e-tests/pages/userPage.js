e2294a661fee77cc07b7248e5b2002b661a95b8a
katsiaryna.yurasova <kateyurasova@gmail.com>
2018-07-10 15:26:22 +0300

    Added allure step

--- e2e-tests/pages/userPage.js
+++ e2e-tests/pages/userPage.js
@@ -5,10 +5,11 @@ var userPage = function () {
     let nameField = element(by.xpath('//*[@id="user-panel"]/li[1]/b/a'))
     let logoutLink = element(by.xpath('//*[@id="user-panel"]/li[7]/a'))
 
-    this.getUserName = async function () {
-        let userName = await nameField.getText()
-        return userName
-    }
+    allure.createStep('user get Test',
+        this.getUserName = async function () {
+            let userName = await nameField.getText()
+            return userName
+        })
 
     this.clickLogOut = async function () {
         await logoutLink.click()
